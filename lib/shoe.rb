40472773c25ee5dbfd8f2bd0baef764bd410ef12
Adam Urbina <adamurbina@gmail.com>
2018-06-05 21:03:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,4 +4,32 @@ class Shoe
     def initialize(brand)
         @brand = brand
     end
+
+    def brand
+        @brand
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    def color
+        @color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size
+    end
+
+    def material=(material)
+        @material
+    end
+
+    def material
+        @material
+    end
 end
